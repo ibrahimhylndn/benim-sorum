de208d9b87fb08a98ee67d4425eafbfc876d13a4
ibrahimhylndn <64245307+ibrahimhylndn@users.noreply.github.com>
2020-05-20 21:52:24 +0100

    Add files via upload

---
+++ Proje/src/proje/Kitaplar.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proje;
+
+/**
+ *
+ * @author darkmac
+ */
+class Kitaplar {
+    
+        String[][] kitaplar1 = new String[][]{
+            
+            {"Ali","Ayse","Mehmet","Selin"},
+            {"Kitap1","Kitap2","Kitap3","Kitap4"},
+            {"Kitap2","Kitap3","Kitap4","Kitap5"},
+            {"Kitap3","Kitap4","Kitap5","Kitap6"},
+            {"Kitap4","Kitap5","Kitap6","Kitap7"}
+            };
+
+    public String Kitaplar(){
+    return null;
+        
+        
+        
+    }
+    
+}
---
+++ Proje/src/proje/Proje.java
@@ -0,0 +1,96 @@
+package proje;
+
+import java.util.Scanner;
+
+
+public class Proje {
+
+    String b;
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        Kitaplar kitaplar = new Kitaplar();
+        String[][] kitaplar1 = kitaplar.kitaplar1;
+        
+        
+        
+        
+        System.out.println(
+                "1 = tum okunan kitaplar\n" +
+                "2 = ozel kisilerin okudugu kitaplar"
+                
+        );
+        int a=scan.nextInt();
+        
+        if(a==1){
+            
+            TumOkunanKitaplar();
+            
+        }
+        
+        if(a==2){
+            
+            
+            OzelKisilerinOkuduguKitaplar();
+                
+            }
+           
+            
+            
+        
+        
+    }
+    
+    private static void TumOkunanKitaplar(){
+        Kitaplar kitaplar = new Kitaplar();
+        String[][] kitaplar1 = kitaplar.kitaplar1;
+        
+        for(int sutun = 0;sutun<kitaplar1[0].length;sutun++){
+            for(int satir = 0;satir<kitaplar1.length;satir++){
+                
+                   System.out.println(kitaplar1[satir][sutun]);
+                    
+                }
+                System.out.println();
+                
+            }
+            
+        }
+        
+     private static void OzelKisilerinOkuduguKitaplar(){
+        Kitaplar kitaplar = new Kitaplar();
+        String[][] kitaplar1 = kitaplar.kitaplar1;
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println(
+                      "Ali "
+                    + "Ayse "
+                    + "Mehmet "
+                    + "Selin");
+        
+        String b=scan.next();
+        
+        
+            
+        for(int sutun = 0;sutun<kitaplar1[0].length;sutun++){
+            
+             if(b.equalsIgnoreCase(kitaplar1[0][sutun])){  
+                      
+                for(int satir = 0;satir<kitaplar1.length;satir++){
+            
+                
+                
+                   System.out.println(kitaplar1[satir][sutun]);
+                }
+                    
+                
+                System.out.println();
+                }
+            }
+        
+    }
+    
+    
+}
+ 
